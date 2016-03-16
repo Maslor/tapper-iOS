81c7b9969e971dcdf22ee18da83a849e5071c6dc
Maslor <gabriel.freire@tecnico.ulisboa.pt>
2016-03-16 13:30:46 +0000

    first commit

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -9,17 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var howManyTapsText: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var tapsLabel: UILabel!
+    
+    @IBAction func onCoinTapped(sender: UIButton) {
+        currentTaps++
+        updateTapsLabel()
+        if isGameOver() {
+            restartGame()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsText.text = ""
+        logoImageView.hidden = false
+        howManyTapsText.hidden = false
+        playButton.hidden = false
+        
+        tapButton.hidden = true
+        tapsLabel.hidden = true
     }
-
-
+    
+    func isGameOver() -> Bool{
+        if(currentTaps == maxTaps){
+            return true
+        }
+        return false
+    }
+    func updateTapsLabel() {
+        tapsLabel.text = "\(currentTaps) Taps"
+    }
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+        
+        if howManyTapsText.text != nil && howManyTapsText.text != "" {
+            
+            logoImageView.hidden = true
+            howManyTapsText.hidden = true
+            playButton.hidden = true
+            
+            tapButton.hidden = false
+            tapsLabel.hidden = false
+            
+            maxTaps = Int(howManyTapsText.text!)!
+            currentTaps = 0
+            
+            updateTapsLabel()
+        }
+    }
 }
 
